4442e70f9ae0d7edd12005d1b8f359c40fd6f3bd
Andrea Severini <andreaseverini94@gmail.com>
2022-04-04 16:34:42 +0200

    JS-Recap.js created - 1st Javascript Recap Section Commit

---
+++ JS-Recap.js
@@ -0,0 +1,13 @@
+/* --------------- UNDERSTANDING LET & CONST ---------------
+Different way to create variables.
+Const -> never changing variable
+Let -> new var, for really variable 
+----------------------------------------------------------*/
+
+var myName = "Andrea";
+console.log(myName);
+myName = "Andrea Severini";
+//With let it will be the same
+
+const name = "Andrea";
+//name = "Andrea Severini" --> you cannot re-declare a const variable
