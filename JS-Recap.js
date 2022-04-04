73d9c0bc94661b954aa844c586bc1116f8af4372
Andrea Severini <andreaseverini94@gmail.com>
2022-04-04 16:48:03 +0200

    2nd Javascript Recap Section - Arrow Functions

--- JS-Recap.js
+++ JS-Recap.js
@@ -10,4 +10,35 @@ myName = "Andrea Severini";
 //With let it will be the same
 
 const name = "Andrea";
+console.log(myName);
 //name = "Andrea Severini" --> you cannot re-declare a const variable
+
+/* --------------- ARROW FUNCTIONS -------------------------
+Different syntax to create functions
+
+normal:
+function myFunc(args) {
+    ...
+}
+
+arrow:
+const myFunc = (args) => {
+    ...
+}
+--> no more issues with the "this" keyword in JS 
+(using context)
+----------------------------------------------------------*/
+
+function printMyName(name) {
+  console.log(`${name} as normal function`);
+}
+printMyName("Andrea");
+
+const printMyNameArrow = (name) => {
+  console.log(`${name} as arrow function`);
+};
+printMyNameArrow("Andrea Severini");
+
+//smarter syntax
+const multiply = (number) => number * 2;
+console.log(multiply(2));
